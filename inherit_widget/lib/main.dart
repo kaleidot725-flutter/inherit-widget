8d32825d750a688eb4ceb5c8c9c430c6612cc61f
Yusuke Katsuragawa <hakodate.katsuragawa.yusuke@gmail.com>
2021-09-21 18:21:15 +0900

    feat: update inherited widget sample

--- inherit_widget/lib/main.dart
+++ inherit_widget/lib/main.dart
@@ -1,115 +1,95 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class Counter {
+  int _count;
+  int get count => _count;
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  Counter(this._count);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  void increment() {
+    _count++;
+  }
+
+  void decrement() {
+    _count--;
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class CounterWidget extends StatefulWidget {
+  Counter counter;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  CounterWidget(this.counter);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  CounterState createState() => CounterState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CounterState extends State<CounterWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return CounterInheritedWidget(
+        counter: widget.counter,
+        child: MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(title: const Text("CounterApp")),
+            body: CounterMessage(),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => increment(),
+              child: const Icon(Icons.add),
+            ),
+          ),
+        ));
+  }
 
-  void _incrementCounter() {
+  void increment() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      widget.counter.increment();
     });
   }
 
+  void decrement() {
+    setState(() {
+      widget.counter.decrement();
+    });
+  }
+}
+
+class CounterMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return Center(
+        child: Text(CounterInheritedWidget.of(context, listen: true)!
+            .counter
+            .count
+            .toString()));
   }
+}
+
+class CounterInheritedWidget extends InheritedWidget {
+  const CounterInheritedWidget({
+    Key? key,
+    required this.counter,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final Counter counter;
+
+  static CounterInheritedWidget? of(
+    BuildContext context, {
+    required bool listen,
+  }) {
+    return listen
+        ? context.dependOnInheritedWidgetOfExactType<CounterInheritedWidget>()
+        : context
+            .getElementForInheritedWidgetOfExactType<CounterInheritedWidget>()
+            ?.widget as CounterInheritedWidget;
+  }
+
+  @override
+  bool updateShouldNotify(CounterInheritedWidget old) =>
+      counter.count != old.counter.count;
+}
+
+void main() {
+  runApp(CounterWidget(Counter(0)));
 }
